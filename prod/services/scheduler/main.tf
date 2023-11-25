63a85fd83908b49792c60823f3461ebc83dedbe0
tth <xyz@foo.com>
2023-11-25 10:37:50 +0800

    Add prod env

---
+++ prod/services/scheduler/main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "tth-prop-analysis-bucket"
+    prefix = "prod/scheduled_job/terraform.tfstate"
+  }
+}
+
+module "scheduled_job" {
+  source = "github.com/tzehon/terraform_modules//services/scheduled-job?ref=v0.0.4"
+
+  project_id     = var.project_id
+  scheduler_name = var.scheduler_name
+  service_name   = var.service_name
+  region         = var.region
+  url            = var.url
+}
