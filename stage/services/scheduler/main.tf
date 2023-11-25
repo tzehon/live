97829b1875f767649eb10afc10d4dd8f4c8c4b61
tth <xyz@foo.com>
2023-11-25 10:24:21 +0800

    Parameterize scheduler name for stage

--- stage/services/scheduler/main.tf
+++ stage/services/scheduler/main.tf
@@ -11,10 +11,11 @@ terraform {
 }
 
 module "scheduled_job" {
-  source = "github.com/tzehon/terraform_modules//services/scheduled-job?ref=v0.0.3"
+  source = "github.com/tzehon/terraform_modules//services/scheduled-job?ref=v0.0.4"
 
-  project_id   = var.project_id
-  service_name = var.service_name
-  region       = var.region
-  url          = var.url
+  project_id     = var.project_id
+  scheduler_name = var.scheduler_name
+  service_name   = var.service_name
+  region         = var.region
+  url            = var.url
 }
