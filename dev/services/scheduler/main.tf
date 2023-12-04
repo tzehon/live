e1974f0e7a0974d3aa4be189d25d36633f78a57a
tth <tzehon.tan@gmail.com>
2023-12-04 17:34:15 -0500

    Add secret manager to destroy flow

--- dev/services/scheduler/main.tf
+++ dev/services/scheduler/main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 module "scheduled_job" {
-  source = "github.com/tzehon/terraform_modules//services/scheduled-job?ref=v0.0.4"
+  source = "github.com/tzehon/terraform_modules//services/scheduled-job?ref=v0.0.5"
 
   project_id     = var.project_id
   scheduler_name = var.scheduler_name
