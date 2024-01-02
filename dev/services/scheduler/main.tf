666a4ae59e1a59831f69e1e0c86a8368a82b9851
tth <xyz@foo.com>
2024-01-02 21:44:43 +0800

    Connect to dev atlas

--- dev/services/scheduler/main.tf
+++ dev/services/scheduler/main.tf
@@ -4,18 +4,24 @@ provider "google" {
 }
 
 module "scheduled_job" {
-  source = "github.com/tzehon/terraform_modules//services/scheduled-job?ref=v0.0.11"
+  source = "github.com/tzehon/terraform_modules//services/scheduled-job?ref=v0.1.0"
   # source = "/Users/tth/projects/terraform_modules/services/scheduled-job"
 
-  project_id     = var.project_id
-  scheduler_name = var.scheduler_name
-  service_name   = var.service_name
-  region         = var.region
-  url            = var.url
-  secret_id      = var.secret_id
-  secret_value   = var.secret_value
-  connector_name = var.connector_name
-  cidr_range     = var.cidr_range
-  router_name    = var.router_name
-  ip_name        = var.ip_name
+  project_id                    = var.project_id
+  scheduler_name                = var.scheduler_name
+  service_name                  = var.service_name
+  region                        = var.region
+  url                           = var.url
+  access_key_id                 = var.access_key_id
+  access_key_value              = var.access_key_value
+  atlas_user_id                 = var.atlas_user_id
+  atlas_user_value              = var.atlas_user_value
+  atlas_password_id             = var.atlas_password_id
+  atlas_password_value          = var.atlas_password_value
+  atlas_connection_string_id    = var.atlas_connection_string_id
+  atlas_connection_string_value = var.atlas_connection_string_value
+  connector_name                = var.connector_name
+  cidr_range                    = var.cidr_range
+  router_name                   = var.router_name
+  ip_name                       = var.ip_name
 }
